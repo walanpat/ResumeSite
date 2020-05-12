641abbf90a8e539cb27729e0e5fdfa043a7a1213
walanpat <39136284+walanpat@users.noreply.github.com>
2020-05-11 23:38:24 -0400

    Update Home.js

--- react_spa/src/Home.js
+++ react_spa/src/Home.js
@@ -4,15 +4,10 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
+        <h2>Hello There</h2>
+        <p>Introduction goes here, talk about yourself and fluff yourself up.  Post a UNC Graduation Photo probably.</p>
  
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+        <p>Put a link to your real resume.</p>
       </div>
     );
   }
