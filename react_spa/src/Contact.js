6a8fa5f4b5d0478b3547e85414b551ca0f023a71
walanpat <39136284+walanpat@users.noreply.github.com>
2020-05-11 23:35:01 -0400

    Initial Start
    Implemented three tabs,
    Home
    Experience
    and Contact.
    
    Need to flesh out Home and CSS this a lot more but it's a start.

---
+++ react_spa/src/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Email</h2>
+        <p>The easiest way reach me is via email at <a href="walanpat@live.unc.edu"> walanpat@live.unc.edu</a>.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
---
+++ react_spa/src/Stuff.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Classes Taken</h2>
+        <br></br>
+     
+          <li>Discrete Structures</li>
+          <li>Foundations of Programming</li>
+          <li>Intro to Programming</li>
+          <li>Data Structures</li>
+          <li>Computer Organization</li>
+          <li>Modern Web Programming</li>
+          <li>Models of Language and Computation</li>
+          <li>Algorithms and Analysis</li>
+    
+        <br></br>        
+        <h2>Personal Projects</h2>
+        <br></br>        
+            <li>Developed simple video games such as Asteroids using Java</li>
+            <li>Oversaw tutoring for databasing classes, foundational programming classes, and introduction to programming skills</li>
+            <li>Currently developing a personal portfolio website using HTML and CSS project work</li>
+
+        <br></br>        
+        <h2>Course Projects</h2>
+        <br></br>          
+            <li>Produced a fully functioning Dijkstra's Algorithm replica using Java on multiple types of Binary Search Trees</li>
+            <li>Produced a functioning copy of the popular number puzzle game 2048 within a 1d array format</li>
+            <li>Have produced a “Hearthstone Cardgame Clone” referred to as CompStone in JS for coursework credit.</li>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
