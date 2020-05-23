c98246892f7f9f477fb240fa0223cacc7c679e68
W. Alan Patterson II <walanpat@live.unc.edu>
2020-05-23 13:48:09 -0400

    Change 2
    Comments have been made outlining what needs to be written/formatting of said page.
    
    Only thing that needs to be done now is to structure the content separate of the website and figure out some way of hosting this (for free atleast)
    
    Maybe put in a link to the UNC RELI and COMP SCI department?

--- react_spa/src/Main.js
+++ react_spa/src/Main.js
@@ -7,24 +7,44 @@ import {
   import Home from "./Home";
   import History from "./History";
   import Symbolism from "./Symbolism";
+  import Sources from "./Sources";
+
   import "./index.css";
+//Home
+  //About me, about the discussion of what this is, BlackMarket/DeepWeb a little.  Talk about interests.
 
+//History
+  // The History of internet security and what not.  
+  //Discuss the silk road, discuss Bitcoin and the other 
 
+//Folklore/symbolism
+  //Talk about what we actually need to talk about. 
+  //Discuss the "What is it in the psyche/symbolistically/what it represents"
+
+//Sources
+  //informal bibliography, for the grad student and requirements.
+
+//?????? Something else maybe?????  Not so sure, maybe a link to UNC RELI department or something just for the fun of it, or to my own github.
+  //We will see what is discussed.
 class Main extends Component {
     render() {
       return (
         <HashRouter>
           <div>
-            <h1>William "Alan" Patterson II</h1>
+            <h1>The Dark Web (informational site)</h1>
             <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
               <li><NavLink to="/history">History</NavLink></li>
               <li><NavLink to="/contact">Folklore/Symbolism</NavLink></li>
+              <li><NavLink to="/sources">Sources</NavLink></li>
+
             </ul>
             <div className="content">
               <Route exact path="/" component={Home}/>
               <Route path="/history" component={History}/>
               <Route path="/symbolism" component={Symbolism}/>
+              <Route path="/sources" component={Sources}/>
+
             </div>
           </div>
         </HashRouter>
---
+++ react_spa/src/Sources.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Sources extends Component {
+    //H2 is our bigger text block, title style
+            //More than likely will be the "title" of any such source used
+
+    // p will be our body text to anything that goes on.
+            //Informationon how to find the source, and other information regarding how to access such things will be listed here.
+
+        
+  render() {
+    return (
+      <div>
+        <h2>Email</h2>
+        <p>The easiest way reach me is via email at <a href="walanpat@live.unc.edu"> walanpat@live.unc.edu</a>.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Sources;
